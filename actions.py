438d33c5dc9b1dcb03abb8f2f27f9a4bcbb8229c
George Doujaiji <GeorgeD.88888@gmail.com>
2023-03-28 23:47:16 -0400

    Rename buttons.py to actions.py
    I renamed buttons to actions because that was a lot more accurate, and
    renamed the variables/constants in the file as well.
    The new names very accurately reflect the purpose of the variables.

---
+++ actions.py
@@ -0,0 +1,9 @@
+""" These constants act as a way to refer to what button is pressed. """
+
+SAVE_MAIN = "SAVE_MAIN"
+
+SAVE_OTHER = "SAVE_OTHER"
+
+UNDO_SAVE = "UNDO_SAVE"
+
+QUIT_APP = "QUIT_APP"
--- buttons.py
+++
@@ -1,9 +0,0 @@
-""" These constants act as a way to refer to what button is pressed. """
-
-MAIN_BUTTON = "MAIN_BUTTON"
-
-OTHER_BUTTON = "OTHER_BUTTON"
-
-UNDO_BUTTON = "UNDO_BUTTON"
-
-QUIT_BUTTON = "QUIT_BUTTON"
