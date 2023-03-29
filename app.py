d657484c156fac14e4d52b5b6045868fa4f26b2b
George Doujaiji <GeorgeD.88888@gmail.com>
2023-03-28 23:50:14 -0400

    Clean up main and add start_input_listener

--- app.py
+++ app.py
@@ -5,11 +5,20 @@ import utils
 
 
 def main():
+    # get main and other playlists through console input
     main_playlist = input('main playlist link: ')
     other_playlist = input('other playlist link: ')
-    # TODO: parse link here; trim if link, and check if valid
 
+    # convert inputs to Spotify playlist IDs if links were given
+    if main_playlist[:4] == 'http':
+        main_playlist = utils.spotify_id_from_link(main_playlist)
+    if other_playlist[:4] == 'http':
+        other_playlist = utils.spotify_id_from_link(other_playlist)
+
+    # initialize the main quick saver component and start the input listener
+    print('starting QuickSave app!')
     quicksaver = QuickSaver(HotKeyListener, utils.spotify_id_from_link(main_playlist), utils.spotify_id_from_link(other_playlist))
+    quicksaver.start_input_listener()
 
 
 if __name__ == "__main__":
